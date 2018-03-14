9fd40d7bfdd50727276f6b0e5ef1a4e99a827a0c
Shanks <dshanks0811@gmail.com>
2018-03-14 16:20:43 -0400

    Forecast model and DAL

--- m3-w09d3-csharp-capstone/Capstone.Web/DAL/ForeCastSQLDAL.cs
+++ m3-w09d3-csharp-capstone/Capstone.Web/DAL/ForeCastSQLDAL.cs
@@ -11,25 +11,31 @@ namespace Capstone.Web.DAL
     public class ForeCastSQLDAL
     {
         public string connectionString = ConfigurationManager.ConnectionStrings["ParkWeather"].ConnectionString;
-        public string SQL_GetForeCast = @"SELECT * FROM weather WHERE ParkCode = @ParkCode";
+        public string SQL_GetForecast = @"SELECT * FROM weather WHERE ParkCode = @ParkCode";
 
-        public List<ForeCast> GetAllParks()
+        public List<ForeCast> GetForecast(string ParkCode)
         {
-            List<ForeCast> fiveDayForeCast = new List<Park>();
+            List<ForeCast> fiveDayForeCast = new List<ForeCast>();
 
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    SqlCommand cmd = new SqlCommand(SQL_GetForeCast, connection);
+                    SqlCommand cmd = new SqlCommand(SQL_GetForecast, connection);
+                    cmd.Parameters.AddWithValue("@ParkCode", ParkCode);
                     SqlDataReader reader = cmd.ExecuteReader();
                     while (reader.Read())
                     {
-
+                        ForeCast f = new ForeCast();
+                        f.FiveDayForecastValue = Convert.ToInt32(reader["fiveDayForecast"]);
+                        f.Low = Convert.ToInt32(reader["low"]);
+                        f.High = Convert.ToInt32(reader["high"]);
+                        f.Forecast = Convert.ToString(reader["forecast"]);
+                        fiveDayForeCast.Add(f);
                     }
                 }
-                return allParks;
+                return fiveDayForeCast;
             }
             catch (SqlException)
             {
